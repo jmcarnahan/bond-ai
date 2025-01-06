ba63b79c31ea2774c8e03e66b4edbb05a6921feb
jmcarnahan <jmcarny@gmail.com>
2025-01-05 21:32:33 -0800

    added more tests

--- tests/test_agent.py
+++ tests/test_agent.py
@@ -1,6 +1,75 @@
-from bond_ai.bond.agent import Agent
+from bond_ai.bond.agent import Agent, AgentResponseHandler
+from bond_ai.bond.threads import Threads
+from bond_ai.bond.config import Config
+
 
 def test_list_agents():
   agents = Agent.list_agents()
   assert len(agents) > 0
-  [print(agent) for agent in agents]
+
+def test_simple_agent():
+  pass
+
+class MyFunctionsAgentHandler(AgentResponseHandler):
+  def __init__(self):
+    self.response = ""
+
+  def on_content(self, content, id, type, role):
+    self.response += " " + content
+
+  def on_done(self, success=True, message=None):
+    pass
+
+
+def test_functions_agent():
+  openai_client = Config.get_openai_client()
+  functions_assistant = openai_client.beta.assistants.create(
+      name="Functions Agent",
+      instructions="""
+      You are a helpful assistant that answers questions about numbers. When the user
+      gives you two numbers, you will use them to call the function 'use_numbers'. The use
+      the result of that function to answer the question.
+      """,
+      tools=[{"type": "code_interpreter"},
+        {
+          "type": "function",
+          "function": {
+            "name": "use_numbers",
+            "description": "use two numbers to generate a result",
+            "parameters": {
+              "type": "object",
+              "properties": {
+                "a": {
+                  "type": "integer",
+                  "description": "first number"
+                },
+                "b": {
+                  "type": "integer",
+                  "description": "second number"
+                },
+              },
+              "required": ["a", "b"]
+            }
+          }
+        }],
+      model=Config.get_openai_deployment(),
+  )
+  assert functions_assistant is not None
+
+  threads = Threads(user_id="test")
+  thread_id = threads.create_thread()
+  assert thread_id is not None
+
+  handler = MyFunctionsAgentHandler()
+  agent = Agent.get_agent_by_name("Functions Agent")
+  assert agent is not None
+  agent.handle_response("The numbers are 3 and 4", thread_id, handler)
+  assert len(handler.response) > 0
+  assert "-1" in handler.response
+  print(handler.response)
+
+  threads.delete_thread(thread_id)
+
+  response = openai_client.beta.assistants.delete(assistant_id=functions_assistant.id)
+  assert response is not None
+  assert response.deleted == True
--- tests/test_config.py
+++ tests/test_config.py
@@ -1,5 +1,27 @@
 from bond_ai.bond.config import Config
+import os
+import sys
+import importlib
 
 def test_openai():
   openai_client = Config.get_openai_client()
   assert openai_client is not None
+
+def test_get_functions():
+  functions = Config.get_functions()
+  assert functions is not None
+
+class MyPages:
+  @classmethod
+  def get_pages(cls):
+    return ['foo', 'bar']
+
+def test_get_pages():
+  #set the env var PAGES_FUNCTION to the string version of the get_pages function in MyPages
+  tests_path = os.path.abspath(os.path.join(os.path.dirname(__file__)))
+  if tests_path not in sys.path:
+      sys.path.append(tests_path)
+  os.environ['PAGES_FUNCTION'] = 'test_config.MyPages.get_pages'
+  print(os.getenv('PAGES_FUNCTION'))
+  pages = Config.get_pages()
+  assert pages is not None
