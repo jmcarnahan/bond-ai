e59f72d25b3221b28a7040501e139377dd12a17a
jmcarnahan <jmcarny@gmail.com>
2025-03-13 22:52:11 -0700

    redid the auth to work with other google accounts

--- bondable/app/index.py
+++ bondable/app/index.py
@@ -1,33 +1,23 @@
-import streamlit as st
-from bondable.bond.pages import Pages
-from bondable.bond.threads import Threads
-from bondable.app.threads_page import ThreadsPage
 import logging
-import os
-import re
-from streamlit_google_auth import Authenticate
-from dotenv import load_dotenv
-
-
-load_dotenv()
-
-LOGGER = logging.getLogger(__name__)
 logging.basicConfig(
     format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
     level=logging.INFO, 
     datefmt="%Y-%m-%d %H:%M:%S",
 )
 logging.getLogger("httpx").setLevel(logging.WARNING)
+LOGGER = logging.getLogger(__name__)
 
-def get_authenticator():
-    if 'authenticator' not in st.session_state:
-        st.session_state['authenticator'] = Authenticate(
-            secret_credentials_path = os.getenv('GOOGLE_AUTH_CREDS_PATH'),
-            cookie_name=os.getenv('GOOGLE_AUTH_COOKIE_NAME', "__bond_ai_name"),
-            cookie_key=os.getenv('GOOGLE_AUTH_COOKIE_KEY', '__bond_ai_key'),
-            redirect_uri=os.getenv('GOOGLE_AUTH_REDIRECT_URI'),
-        )
-    return st.session_state['authenticator']
+
+import streamlit as st
+from bondable.bond.pages import Pages
+from bondable.bond.threads import Threads
+from bondable.app.threads_page import ThreadsPage
+from bondable.app.auth import StreamlitAuth
+import os
+import re
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 def display_page(page): 
@@ -79,35 +69,30 @@ def create_threads_page():
         return page.display_threads()
     return threads_page
 
+def create_logout_page(auth: StreamlitAuth):
+    def logout_page():
+        if st.button("Logout"):
+            st.session_state["user"] = None
+            auth.delete_cookie()
+            st.rerun()
+    return logout_page
 
-def create_google_login_page():
+def create_google_login_page(auth: StreamlitAuth):
     def login_page():
-        authenticator = get_authenticator()
-        authenticator.check_authentification()
-        authenticator.login()
-    return login_page
+        st.title("Login")
+        LOGGER.debug("User is not logged in")
+        st.write("Click the button below to log in:")
+        auth_url = auth.get_auth_url()
 
-def create_simple_login_page():
-    def process_email():
-        email = st.session_state["email_input"]
-        if re.match(r"[^@]+@[^@]+\.[^@]+", email):
-            st.session_state['connected'] = True
-            st.session_state['user_info'] = {'name': "Guest", 'email': email}
-            LOGGER.info(f"Connected as {email}")
-        else:
-            st.error("Please enter a valid email")
-
-    def login_page():
-        #st.markdown("#### Enter your email address")
-        col1, col2, col3 = st.columns([1, 2, 1])
-        with col2:
-            email = st.text_input("Enter your email", key="email_input", on_change=process_email)
-            if st.button('Enter'):
-                process_email()
+        st.markdown(f"""
+    <a href="{auth_url}" target="_self">
+        <img src="https://developers.google.com/identity/images/btn_google_signin_dark_normal_web.png" width="200">
+    </a>
+    """, unsafe_allow_html=True)
     return login_page
 
 
-def main_pages(name, user_id):
+def main_pages(auth: StreamlitAuth, name, user_id):
     pages = {}
 
     st.session_state['user_id'] = user_id
@@ -116,6 +101,7 @@ def main_pages(name, user_id):
     account = []
     account.append(st.Page(create_home_page(name=name, pages=agent_pages), title="Home"))
     account.append(st.Page(create_threads_page(), title="Threads"))
+    account.append(st.Page(create_logout_page(auth=auth), title="Logout"))
     pages["Account"] = account
 
     # thread_id = config.get_threads().get_current_thread_id()
@@ -128,32 +114,26 @@ def main (name, email):
     pg = st.navigation(pages)
     pg.run()
 
-
-def login_main():
+def login_main(auth: StreamlitAuth):
     pages = {}
-    if 'connected' in st.session_state and st.session_state['connected']:
-        name  = st.session_state['user_info'].get('name')
-        email = st.session_state['user_info'].get('email')
-        pages = main_pages(name, email)
-
+    if auth.bond_login():
+        LOGGER.info(f"Logged in: {st.session_state['user']['name']}")
+        pages = main_pages(auth=auth, name=st.session_state["user"]['name'], user_id=st.session_state["user"]['email'])
     else:
-        if os.getenv('GOOGLE_AUTH_ENABLED', "False").lower() == "true":
-            LOGGER.info(f"Starting app with login with redirect: {os.getenv('GOOGLE_AUTH_REDIRECT_URI')}")
-            pages = {'Login': [st.Page(create_google_login_page(), title="Login")]}
-        else:
-            LOGGER.info("Using simple login without google auth")
-            pages = {'Login': [st.Page(create_simple_login_page(), title="Login")]}
+        LOGGER.debug(f"Showing login page")
+        pages = {'Login': [st.Page(create_google_login_page(auth), title="Login")]}
 
+    LOGGER.debug(f"Starting app with pages: {pages}")
     pg = st.navigation(pages)
     pg.run()
-    
+
 
 if __name__ == "__main__":
     st.set_page_config(page_title="Home", layout="wide")
+    auth: StreamlitAuth = StreamlitAuth.auth()
     if os.getenv('AUTH_ENABLED', "True").lower() == "true":
-        login_main()
+        login_main(auth)
     else:
         main("", "Guest")
-
 
 
