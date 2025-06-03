c962c1aa87c72b857e27a5549fc0fb82b3a039d2
jmcarnahan <jmcarny@gmail.com>
2025-06-03 16:23:06 -0700

    fixed a core problem with file search

--- bondable/bond/definition.py
+++ bondable/bond/definition.py
@@ -35,6 +35,9 @@ class AgentDefinition:
         self.config = Config.config()
         self.provider = self.config.get_provider()
 
+        if user_id is None:
+            raise ValueError("User ID must be provided for agent definition.")
+
         # load the tools
         self.tools = []
         for i, tool in enumerate(tools):
@@ -70,17 +73,29 @@ class AgentDefinition:
 
         if "file_search" in self.tool_resources and self.tool_resources["file_search"] is not None:
             vector_store_ids = []
+
+            default_vector_store_name = f"{self.name}_file_search_vs"
+            default_vector_store_id = self.provider.vectorstores.get_or_create_vector_store_id(user_id=user_id, name=default_vector_store_name)
+            vector_store_ids.append(default_vector_store_id)
+
             if "vector_store_ids" in self.tool_resources["file_search"] and self.tool_resources["file_search"]["vector_store_ids"] is not None:
-                vector_store_ids = self.tool_resources["file_search"]["vector_store_ids"]
+                vector_store_ids.extend(self.tool_resources["file_search"]["vector_store_ids"])
+                
+            if 'file_ids' in self.tool_resources["file_search"] and self.tool_resources["file_search"]["file_ids"] is not None:
+                self.provider.vectorstores.update_vector_store_file_ids(vector_store_id=default_vector_store_id, file_ids=self.tool_resources["file_search"]["file_ids"])
+
             if "files" in self.tool_resources["file_search"]:
-                default_vector_store_name = f"{self.name}_file_search_vs"
-                default_vector_store_id = self.provider.vectorstores.get_or_create_vector_store_id(
-                    user_id=user_id, name=default_vector_store_name, file_tuples=self.tool_resources["file_search"]["files"])
-                if default_vector_store_id not in vector_store_ids:
-                    vector_store_ids.append(default_vector_store_id)
+                file_ids = []
+                for file_tuple in self.tool_resources["file_search"]["files"]:
+                    file_id = self.provider.files.get_or_create_file_id(user_id=user_id, file_tuple=file_tuple)
+                    if file_id not in file_ids:
+                        file_ids.append(file_id)
+                self.provider.vectorstores.update_vector_store_file_ids(vector_store_id=default_vector_store_id, file_ids=file_ids)
+
             self.tool_resources["file_search"] = {
                 "vector_store_ids": list(set(vector_store_ids))
             }
+
         else:
             # Ensure the structure exists even if no resources were provided
             self.tool_resources["file_search"] = {"vector_store_ids": []}
