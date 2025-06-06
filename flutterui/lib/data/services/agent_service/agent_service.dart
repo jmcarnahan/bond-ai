61aa514288f42c3349a2fb4f7a26582df37804c9
jmcarnahan <jmcarny@gmail.com>
2025-06-06 11:05:23 -0700

    moved file service out of agent

--- flutterui/lib/data/services/agent_service/agent_service.dart
+++ flutterui/lib/data/services/agent_service/agent_service.dart
@@ -1,5 +1,3 @@
-import 'dart:typed_data';
-import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 
 import 'package:flutterui/data/models/agent_model.dart';
@@ -7,19 +5,15 @@ import 'package:flutterui/data/models/api_response_models.dart';
 import 'package:flutterui/data/services/auth_service.dart';
 import 'agent_http_client.dart';
 import 'agent_crud_service.dart';
-import 'agent_file_service.dart';
 
 class AgentService {
   final AgentCrudService _crudService;
-  final AgentFileService _fileService;
   final AgentHttpClient _httpClient;
 
   AgentService._({
     required AgentCrudService crudService,
-    required AgentFileService fileService,
     required AgentHttpClient httpClient,
   })  : _crudService = crudService,
-        _fileService = fileService,
         _httpClient = httpClient;
 
   factory AgentService({
@@ -32,11 +26,9 @@ class AgentService {
     );
     
     final crudService = AgentCrudService(httpClient: agentHttpClient);
-    final fileService = AgentFileService(httpClient: agentHttpClient);
     
     return AgentService._(
       crudService: crudService,
-      fileService: fileService,
       httpClient: agentHttpClient,
     );
   }
@@ -53,20 +45,6 @@ class AgentService {
       _crudService.updateAgent(agentId, agentData);
 
   Future<void> deleteAgent(String agentId) => _crudService.deleteAgent(agentId);
-
-  Future<FileUploadResponseModel> uploadFile(String fileName, Uint8List fileBytes) => 
-      _fileService.uploadFile(fileName, fileBytes);
-
-  Future<void> deleteFile(String providerFileId) => 
-      _fileService.deleteFile(providerFileId);
-
-  Future<List<FileInfoModel>> getFiles() => _fileService.getFiles();
-
-  Future<FileInfoModel> getFileInfo(String providerFileId) => 
-      _fileService.getFileInfo(providerFileId);
-
-  Future<List<FileDetailsResponseModel>> getFileDetails(List<String> fileIds) => 
-      _fileService.getFileDetails(fileIds);
 
   void dispose() {
     _httpClient.dispose();
