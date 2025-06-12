3c13a699f8412b866f1a93a899fed1a65cdb2c1d
jmcarnahan <jmcarny@gmail.com>
2025-06-12 07:38:31 -0700

    removed old id

--- flutterui/lib/core/constants/mobile_api_config.dart
+++ flutterui/lib/core/constants/mobile_api_config.dart
@@ -4,9 +4,9 @@ class MobileApiConfig {
   static String get baseUrl =>
       dotenv.env['API_BASE_URL'] ?? 'http://localhost:8000';
 
-  static String get mobileAgentId =>
-      dotenv.env['MOBILE_AGENT_ID'] ?? 'asst_BZ6WaTaNc0THp8eCSDx6leHD';
+  static String get defaultAgentId =>
+      dotenv.env['DEFAULT_AGENT_ID'] ?? 'default-agent';
 
-  static String get mobileAgentName =>
-      dotenv.env['MOBILE_AGENT_NAME'] ?? 'My Companion';
+  static String get defaultAgentName =>
+      dotenv.env['DEFAULT_AGENT_NAME'] ?? 'My Companion';
 }
