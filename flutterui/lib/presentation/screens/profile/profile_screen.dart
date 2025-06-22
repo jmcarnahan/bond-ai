c725b4b9b4a6b410c099262193b10f3236c67348
jmcarnahan <jmcarny@gmail.com>
2025-06-21 22:53:30 -0700

    refactor: remove unused app_theme import from profile_screen.dart

--- flutterui/lib/presentation/screens/profile/profile_screen.dart
+++ flutterui/lib/presentation/screens/profile/profile_screen.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../../../providers/auth_provider.dart';
-import '../../../core/theme/app_theme.dart';
 import 'widgets/profile_app_bar.dart';
 import '../../widgets/app_drawer.dart';
 
