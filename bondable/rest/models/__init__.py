2464d8d343c9cbfdc39c2e29067b89c391636fa3
jmcarnahan <jmcarny@gmail.com>
2026-01-06 10:05:55 -0800

    Refactor whitespace for improved readability in __init__.py, auth.py, agents.py, files.py, groups.py, and threads.py

--- bondable/rest/models/__init__.py
+++ bondable/rest/models/__init__.py
@@ -11,17 +11,17 @@ from .files import FileUploadResponse, FileDeleteResponse
 __all__ = [
     # Auth models
     "Token", "User",
-    
+
     # Agent models
     "AgentRef", "ToolResourceFilesList", "ToolResourcesRequest", "ToolResourcesResponse",
     "AgentCreateRequest", "AgentUpdateRequest", "AgentResponse", "AgentDetailResponse",
-    
+
     # Thread models
     "ThreadRef", "CreateThreadRequest", "MessageRef",
-    
+
     # Chat models
     "ChatRequest",
-    
+
     # File models
     "FileUploadResponse", "FileDeleteResponse",
 ]
